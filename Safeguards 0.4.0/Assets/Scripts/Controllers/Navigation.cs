d87e467b247e8dd8ffbb4be7101e6737d11bda3c
DESKTOP-LHFR5BH\Andy <richtertest@gmail.com>
2019-04-15 20:31:07 -0400

    Interface Actions, Reorganizing
    Implemented interfaces on my actions.  Organized the data into one class, moving away from Scriptable Objects.  Now there will be an "action holder" object to which we will attach the components for the different actions when needed.  units will have action lists.
    
    added data classes such as Item, Weapon, AttackData.
    
    Fleshed out the NormalAttack class.  Still WIP.

---
+++ Safeguards 0.4.0/Assets/Scripts/Controllers/Navigation.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Navigation : MonoBehaviour
+{
+    NavMeshAgent agent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                UnitData target = hit.collider.GetComponent<UnitData>();
+                if (target != null && PlayerPrefs.GetString("selected", "none").Equals(target.unit_name))
+                {
+                    agent.destination = hit.point;
+                }
+                
+            }
+        }
+        if (Input.GetMouseButtonDown(1))
+        { // if right button pressed...
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                //if (cam.selected.Equals(GetComponent<Data_Manager>().character_name))
+                UnitData target = hit.collider.GetComponent<UnitData>();
+                if (target != null && GameController.master.selected.Equals(target.unit_name))
+                {
+                    PlayerPrefs.SetString("selected", "none"); ;
+                    Debug.Log("Deselect");
+                }
+                else
+                {
+                    //PlayerPrefs.SetString("selected", hit.collider.GetComponent<Data_Manager>().character_name);
+                    //cam.selected = hit.collider.GetComponent<Data_Manager>().character_name; //something about this is not working right. it won't let Hadrian be selected unless Fant already was.
+                    Debug.Log("Select");
+                }
+                
+            }
+        }
+    }
+}
---
+++ Safeguards 0.4.0/Assets/Scripts/Data/AttackData.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackData
+{
+    public bool executed = false;
+    public bool hit = false;
+    public bool crit = false; //these are for logging purposes.
+    public int HitRate;
+    public int CriticalRate;
+    public int GuardRate;
+    public int DamageDealt;
+    public string[] OnHitEffects;
+}
